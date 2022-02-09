2ce6f372c480599791f2429320a8e05fbc0ee1f2
yosrimhamdi <yosrimhamdi@outlook.com>
2022-02-09 17:36:26 +0100

    Creating different user types.

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    private $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $superAdminUser = new User();
+        $superAdminUser
+            ->setEmail('yosri@cosavostra.com')
+            ->setPassword(
+                $this->hasher->hashPassword($superAdminUser, 'password')
+            )
+            ->setRoles(['ROLE_SUPER_ADMIN']);
+
+        $user = new User();
+        $user
+            ->setEmail('user@company.com')
+            ->setPassword(
+                $this->hasher->hashPassword($superAdminUser, 'password')
+            );
+
+        $manager->persist($user);
+        $manager->persist($superAdminUser);
+
+        $manager->flush();
+    }
+}
