3fe767bc849fc7802fcfa2ff1e0b5de4952ca714
yosrimhamdi <yosrimhamdi@outlook.com>
2022-02-10 10:21:11 +0100

    Exposing the client resource.

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -2,11 +2,13 @@
 
 namespace App\Entity;
 
-use App\Repository\ClientRepository;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\ClientRepository;
+use ApiPlatform\Core\Annotation\ApiResource;
 
 /**
  * @ORM\Entity(repositoryClass=ClientRepository::class)
+ * @ApiResource()
  */
 class Client
 {
